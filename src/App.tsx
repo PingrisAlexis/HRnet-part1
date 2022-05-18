3ff4586c2fb8e46e6d4ce3b40069d988ff028532
Alexis <pingrisalexis@gmail.com>
2022-05-18 19:24:44 +0200

    Create routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Header, Footer } from './components';
+import {CreateEmployee, EmployeeDirectory} from "./views";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <BrowserRouter>
+            <Header/>
+            <Routes>
+                <Route path="/" element={<CreateEmployee />} />
+                <Route path="/employees" element={<EmployeeDirectory />} />
+            </Routes>
+            <Footer />
+        </BrowserRouter>
     </div>
   );
 }
