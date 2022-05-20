873d3cf6853fe3a2c6793ff6056f51db3c8c376e
Alexis <pingrisalexis@gmail.com>
2022-05-20 16:57:43 +0200

    add employee list

---
+++ src/views/employee-list/EmployeeList.tsx
@@ -0,0 +1,12 @@
+
+import react from "react";
+
+const EmployeeList = () => {
+    return (
+        <main>
+            <h2>Employee List</h2>
+        </main>
+    )
+}
+
+export default EmployeeList;
