e3c0b14c9b6b99b22d2001ed3ae805aa4a3c2169
Alexis <pingrisalexis@gmail.com>
2022-05-24 18:20:39 +0200

    Add Store

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,26 @@ import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore, applyMiddleware, Store } from "redux"
+import { Provider } from "react-redux"
+import thunk from "redux-thunk"
+import {DispatchType, EmployeeAction, EmployeeState} from "./type.d";
+import reducer from "./store/reducer";
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+const store: Store<EmployeeState, EmployeeAction> & {
+    dispatch: DispatchType
+} = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)))
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <App />
+      <Provider store={store}>
+          <App />
+      </Provider>,
   </React.StrictMode>
 );
 
