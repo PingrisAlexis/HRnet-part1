da7db945a007ae55f96e8646d7bea12a113d2a4f
Alexis <pingrisalexis@gmail.com>
2022-05-26 18:53:20 +0200

    Create mock select options

---
+++ src/utils/departments-options/DepartmentsOptions.tsx
@@ -0,0 +1,18 @@
+export const DEPARTMENTS_OPTIONS = [
+    {
+        "label": "Sales",
+        "value": "Sales"
+    },{
+        "label": "Marketing",
+        "value": "Marketing"
+    },{
+        "label": "Engineering",
+        "value": "Engineering"
+    },{
+        "label": "Human Resources",
+        "value": "Human Resources"
+    },{
+        "label": "Legal",
+        "value": "Legal"
+    },
+]
---
+++ src/utils/index.tsx
@@ -0,0 +1,2 @@
+export { US_STATES_OPTIONS } from "./us-states-options/UsStatesOptions";
+export { DEPARTMENTS_OPTIONS } from "./departments-options/DepartmentsOptions";
