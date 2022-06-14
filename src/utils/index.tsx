62aae60b249115f270cb834f5d5f4fb714f5cfde
Alexis <pingrisalexis@gmail.com>
2022-06-14 19:47:57 +0200

    White-spaces

--- src/utils/index.tsx
+++ src/utils/index.tsx
@@ -1,4 +1,4 @@
-export {US_STATES_SELECT_OPTIONS} from "./us-states-select-options/UsStatesOptions";
-export {DEPARTMENTS_SELECT_OPTIONS} from "./departments-select-options/DepartmentsSelectOptions";
-export {customStylesReactSelect} from "./custom-styles-react-select/CustomStylesReactSelect";
-export {columns} from "./custom-table/CustomTable";
+export { US_STATES_SELECT_OPTIONS } from "./us-states-select-options/UsStatesOptions";
+export { DEPARTMENTS_SELECT_OPTIONS } from "./departments-select-options/DepartmentsSelectOptions";
+export { customStylesReactSelect } from "./custom-styles-react-select/CustomStylesReactSelect";
+export { columns } from "./custom-table/CustomTable";
